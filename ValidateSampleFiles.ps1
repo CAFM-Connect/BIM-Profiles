df6c5554eb9d35dbfe993cd750e2a12db4ce85cd
Klaus Aengenvoort <klaus.aengenvoort@etask.de>
2019-02-12 16:16:58 +0100

    Validate the profile files with the IFC SchemaValidator #17

---
+++ ValidateSampleFiles.ps1
@@ -0,0 +1,22 @@
+﻿$BaseFolder = "C:\projects\productdata"
+$ValidatorExe="$BaseFolder\Tools\SchemaValidator\bin\Release\net47\SchemaValidator.exe"
+$SampleFiles = Get-ChildItem -Path $BaseFolder  -Recurse -Include *.ifc,*.ifcXML
+$ExitCode=0
+foreach ($file in $SampleFiles | Sort-Object -Property FullName) 
+{
+   Write-Host "++++++++++++++++++++++++++++++++++++++"
+   $filename = $file.FullName.Replace($BaseFolder,"")
+   Write-Host Validating $filename
+   & $ValidatorExe $file.FullName
+   if ($LastExitCode -eq '0') 
+   { 
+     Write-Host "OK: $filename is valid" -ForegroundColor Green
+   }
+   else 
+   {
+     Write-Host "ERROR: $filename is not valid" -ForegroundColor Red
+     Write-Host "Please check the validation errors and correct them. Thanks for your contribution!" -ForegroundColor Red
+     $ExitCode=1
+   }
+}
+Exit $ExitCode
